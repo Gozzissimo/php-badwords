7aee0df06f7a4ba80d74320342d6328998614388
Gozzissimo <fantini.nico@gmail.com>
2022-01-28 19:21:00 +0100

    Sosittuzione con parola da censurare

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
+<!-- // Descrizione:
+// Creare una variabile con un paragrafo di testo a vostra scelta.
+// Stampare a schermo il paragrafo e la sua lunghezza.
+// Una parola da censurare viene passata dall’utente tramite parametro GET.
+// Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
 <?php
-$filastrocca = "Apelle, figlio di Apollo, fece una palla di pelle di pollo. Tutti i pesci vennero a galla, per vedere la palla di pelle di pollo, fatta da Apelle, figlio di Apollo."
+    $filastrocca = "Apelle, figlio di Apollo, fece una palla di pelle di pollo. Tutti i pesci vennero a galla, per vedere la palla di pelle di pollo, fatta da Apelle, figlio di Apollo.";
+
+    // parola da censurare
+    $parolaCensurata = $_GET["word"];
+
+    $censoredFilastrocca = str_replace("$parolaCensurata", "***", trim($filastrocca));
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +24,11 @@ $filastrocca = "Apelle, figlio di Apollo, fece una palla di pelle di pollo. Tutt
     <title>Document</title>
 </head>
 <body>
-    <?php echo $filastrocca; ?>
+    <p><?= $filastrocca ?></p>
+    <p><?= "Contiene ".strlen($filastrocca)." caratteri" ?></p>
+
+    <p><?= $censoredFilastrocca ?></p>
+    <p><?= "Contiene ".strlen($censoredFilastrocca)." caratteri" ?></p>
+    
 </body>
 </html>
