4cf04bd62b2f25274718ddd60fd7942912434374
Gozzissimo <fantini.nico@gmail.com>
2022-01-28 17:20:59 +0100

    stampa in pagina paragrafo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$filastrocca = "Apelle, figlio di Apollo, fece una palla di pelle di pollo. Tutti i pesci vennero a galla, per vedere la palla di pelle di pollo, fatta da Apelle, figlio di Apollo."
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $filastrocca; ?>
+</body>
+</html>
